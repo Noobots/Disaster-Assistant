1ba1bf330274c813042808e4ac9c373bd51ea6a3
akhila <yesantha@hotmail.com>
2017-11-05 20:11:12 +0530

    dismain

--- disaster-assistant/DisMainPage.xaml.cs
+++ disaster-assistant/DisMainPage.xaml.cs
@@ -20,30 +20,26 @@ namespace disaster_assistant
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class DisMainPage : Page
     {
-        public MainPage()
+        public DisMainPage()
         {
             this.InitializeComponent();
         }
-
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
-
         private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (HomeIcon.IsSelected)
             {
                 this.Frame.Navigate(typeof(MainPage), null);
             }
-            else if(Disaster.IsSelected)
-            {
-                this.Frame.Navigate(typeof(DisMainPage), null);
-            }
+           
         }
 
-      
+
+
     }
 }
