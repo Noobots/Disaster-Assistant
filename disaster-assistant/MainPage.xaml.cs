de50db89b58925af66cc3ffe6ea500c428b2621a
akhila <yesantha@hotmail.com>
2017-11-09 00:13:36 +0530

    login

--- disaster-assistant/MainPage.xaml.cs
+++ disaster-assistant/MainPage.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
+using login;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace disaster_assistant
@@ -38,10 +26,12 @@ namespace disaster_assistant
             {
                 this.Frame.Navigate(typeof(MainPage), null);
             }
-            else if(Disaster.IsSelected)
+            else if (Disaster.IsSelected)
             {
                 this.Frame.Navigate(typeof(DisMainPage), null);
             }
+            else if (Login.IsSelected)
+                this.Frame.Navigate(typeof(login.views.loginmain), null);
         }
 
       
